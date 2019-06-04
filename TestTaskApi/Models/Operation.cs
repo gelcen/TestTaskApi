89840d55825c6854466ac070cd2e6182ca2f842b
Гелцен Савындай <5842_sgy@keva.int>
2019-06-04 19:32:07 +0700

    Added UnitTest project

--- TestTaskApi/Models/Operation.cs
+++ TestTaskApi/Models/Operation.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TestTaskApi.Models
+﻿namespace TestTaskApi.Models
 {
+    /// <summary>
+    /// Entity that helps to realize 
+    /// Add/Withdraw operations with the balance
+    /// </summary>
     public class Operation
     {
+        /// <summary>
+        /// Operation type to realize
+        /// </summary>
         public OperationType OperationType { get; set; }
 
+        /// <summary>
+        /// Amount of money 
+        /// </summary>
         public decimal Sum { get; set; }
     }
 }
